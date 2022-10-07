139c65ec264fa9274bccee9da23d34ac94c04c49
surajb2310 <115218337+surajb2310@users.noreply.github.com>
2022-10-07 14:32:49 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,24 @@
+
+public class abc {
+public static void main(String[] args) {
+	
+	int a[]= {12,456,56,8,78};
+	
+	for(int i=0;i<=a.length-1;i++) {
+		System.out.print(a[i]+" ");
+		
+	}
+	
+	
+	
+	for(int j :a) {
+		
+		System.out.print(a[j]+" ");
+		
+	}
+	
+	
+	
+	
+}
+}
